67abb051f84591b2ae821e6eb956f89a09819a1c
Houzair Koussa <houzairmk@icloud.com>
2023-02-14 18:52:54 +0400

    [dev] REINIT react

--- react-image-component/src/index.js
+++ react-image-component/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ImageComponent from './Image-Component';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ImageComponent />
   </React.StrictMode>
 );
 
